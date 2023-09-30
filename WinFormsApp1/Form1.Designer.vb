f512980e434ef9b71ba0fa953fb396bfa8053607
AziqLukman <aziqlukman10@gmail.com>
2023-09-30 11:38:55 +0700

    konek database mbe crud VS

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        txtnpm = New TextBox()
+        txtnama = New TextBox()
+        txtalamat = New TextBox()
+        txtjenis_kelamin = New ComboBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        DataGridView1 = New DataGridView()
+        Id = New DataGridViewTextBoxColumn()
+        Column1 = New DataGridViewTextBoxColumn()
+        Column2 = New DataGridViewTextBoxColumn()
+        Column3 = New DataGridViewTextBoxColumn()
+        Column4 = New DataGridViewTextBoxColumn()
+        txtid = New TextBox()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(55, 52)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(41, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "NPM"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(55, 100)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(49, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Nama"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(55, 143)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(57, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Alamat"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(55, 185)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(98, 20)
+        Label4.TabIndex = 3
+        Label4.Text = "Jenis Kelamin"
+        ' 
+        ' txtnpm
+        ' 
+        txtnpm.Location = New Point(168, 52)
+        txtnpm.Name = "txtnpm"
+        txtnpm.Size = New Size(485, 27)
+        txtnpm.TabIndex = 4
+        ' 
+        ' txtnama
+        ' 
+        txtnama.Location = New Point(168, 100)
+        txtnama.Name = "txtnama"
+        txtnama.Size = New Size(485, 27)
+        txtnama.TabIndex = 5
+        ' 
+        ' txtalamat
+        ' 
+        txtalamat.Location = New Point(168, 143)
+        txtalamat.Name = "txtalamat"
+        txtalamat.Size = New Size(485, 27)
+        txtalamat.TabIndex = 6
+        ' 
+        ' txtjenis_kelamin
+        ' 
+        txtjenis_kelamin.FormattingEnabled = True
+        txtjenis_kelamin.Items.AddRange(New Object() {"Laki-Laki", "Perempuan"})
+        txtjenis_kelamin.Location = New Point(168, 185)
+        txtjenis_kelamin.Name = "txtjenis_kelamin"
+        txtjenis_kelamin.Size = New Size(485, 28)
+        txtjenis_kelamin.TabIndex = 7
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(215, 237)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 8
+        Button1.Text = "Simpan"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(332, 237)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 9
+        Button2.Text = "Edit"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(463, 237)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 10
+        Button3.Text = "Hapus"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(685, 114)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 29)
+        Button4.TabIndex = 12
+        Button4.Text = "Cek"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {Id, Column1, Column2, Column3, Column4})
+        DataGridView1.Location = New Point(99, 284)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.RowTemplate.Height = 29
+        DataGridView1.Size = New Size(554, 168)
+        DataGridView1.TabIndex = 11
+        ' 
+        ' Id
+        ' 
+        Id.HeaderText = "Id"
+        Id.MinimumWidth = 6
+        Id.Name = "Id"
+        Id.Visible = False
+        Id.Width = 125
+        ' 
+        ' Column1
+        ' 
+        Column1.HeaderText = "NPM"
+        Column1.MinimumWidth = 6
+        Column1.Name = "Column1"
+        Column1.Width = 125
+        ' 
+        ' Column2
+        ' 
+        Column2.HeaderText = "Nama"
+        Column2.MinimumWidth = 6
+        Column2.Name = "Column2"
+        Column2.Width = 125
+        ' 
+        ' Column3
+        ' 
+        Column3.HeaderText = "Jenis Kelamin"
+        Column3.MinimumWidth = 6
+        Column3.Name = "Column3"
+        Column3.Resizable = DataGridViewTriState.True
+        Column3.Width = 125
+        ' 
+        ' Column4
+        ' 
+        Column4.HeaderText = "Alamat"
+        Column4.MinimumWidth = 6
+        Column4.Name = "Column4"
+        Column4.Width = 125
+        ' 
+        ' txtid
+        ' 
+        txtid.Location = New Point(168, 12)
+        txtid.Name = "txtid"
+        txtid.Size = New Size(485, 27)
+        txtid.TabIndex = 13
+        txtid.Visible = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtid)
+        Controls.Add(Button4)
+        Controls.Add(DataGridView1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(txtjenis_kelamin)
+        Controls.Add(txtalamat)
+        Controls.Add(txtnama)
+        Controls.Add(txtnpm)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtnpm As TextBox
+    Friend WithEvents txtnama As TextBox
+    Friend WithEvents txtalamat As TextBox
+    Friend WithEvents txtjenis_kelamin As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Id As DataGridViewTextBoxColumn
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents txtid As TextBox
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,151 @@
+﻿Imports System.Data.Common
+Imports System.Data.Odbc
+Imports System.Data.SqlClient
+Imports Microsoft.SqlServer
+Imports MySql.Data.MySqlClient
+Public Class Form1
+    'Public conn As OdbcConnection
+    'Public cmd As OdbcCommand
+    'Public rd As OdbcDataReader
+    Dim conn As MySqlConnection
+    Dim dr As MySqlDataReader
+    Dim da As MySqlDataAdapter
+    Dim cmd As MySqlCommand
+
+    Sub koneksi()
+        conn = New MySqlConnection("server=127.0.0.1;userid=root;password='';
+        database=db_latihan")
+        Try
+            If conn.State = ConnectionState.Closed Then
+                conn.Open()
+                MsgBox("Koneksi Berhasil", MsgBoxStyle.Exclamation, "informasi")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Koneksi Gagal" & ex.Message)
+        End Try
+    End Sub
+    Sub bersihkan()
+        txtid.Clear()
+        txtnpm.Clear()
+        txtnama.Clear()
+        txtalamat.Clear()
+        'jenis_kelamin.Clear()
+    End Sub
+    Sub tambahdata()
+        Try
+            Call koneksi()
+            Dim input_id As String = txtid.Text
+            Dim input_npm As String = txtnpm.Text
+            Dim input_nama As String = txtnama.Text
+            Dim input_alamat As String = txtalamat.Text
+            Dim input_jenis_kelamin As String = txtjenis_kelamin.Text
+
+            Dim query As String = "INSERT INTO tbl_mahasiswa (id, npm, nama, jenis_kelamin, alamat) VALUES (?,?,?,?,?)"
+
+            cmd = New MySqlCommand(query, conn)
+            cmd.Parameters.AddWithValue("id", input_id)
+            cmd.Parameters.AddWithValue("npm", input_npm)
+            cmd.Parameters.AddWithValue("nama", input_nama)
+            cmd.Parameters.AddWithValue("jenis_kelamin", input_jenis_kelamin)
+            cmd.Parameters.AddWithValue("alamat", input_alamat)
+            cmd.ExecuteNonQuery()
+            MsgBox("Data berhasil disimpan ke database!!")
+            tampildata()
+            bersihkan()
+        Catch ex As Exception
+            MsgBox("Terjadi kesalahan: " & ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+    Sub tampildata()
+        Call koneksi()
+        cmd = New MySqlCommand("Select * from tbl_mahasiswa", conn)
+        dr = cmd.ExecuteReader
+        DataGridView1.Rows.Clear()
+
+        Do While dr.Read = True
+            DataGridView1.Rows.Add(dr(0), dr(1), dr(2), dr(3), dr(4))
+        Loop
+    End Sub
+    Sub hapusdata()
+        Call koneksi()
+        Dim input_id As String = txtid.Text
+        Dim sql As String = "DELETE FROM tbl_mahasiswa WHERE id = ?"
+        Try
+            Dim cmd As New MySqlCommand(sql, conn)
+            cmd.Parameters.AddWithValue("id", input_id)
+            cmd.ExecuteNonQuery()
+            MsgBox("Data berhasil dihapus")
+        Catch ex As Exception
+            MsgBox("Terjadi kesalahan: " & ex.Message)
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        txtid.Text = DataGridView1.Rows(e.RowIndex).Cells(0).Value
+        txtnpm.Text = DataGridView1.Rows(e.RowIndex).Cells(1).Value
+        txtnama.Text = DataGridView1.Rows(e.RowIndex).Cells(2).Value
+        txtalamat.Text = DataGridView1.Rows(e.RowIndex).Cells(3).Value
+        txtjenis_kelamin.Text = DataGridView1.Rows(e.RowIndex).Cells(4).Value
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Call koneksi()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call tambahdata()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If DataGridView1.SelectedRows.Count > 0 Then
+            Dim selectedRow As DataGridViewRow = DataGridView1.SelectedRows(0)
+            Dim idToUpdate As Integer = Convert.ToInt32(selectedRow.Cells("id").Value.ToString())
+
+            Dim input_npm As String = txtnpm.Text
+            Dim input_nama As String = txtnama.Text
+            Dim input_alamat As String = txtalamat.Text
+            Dim input_jenis_kelamin As String = txtjenis_kelamin.Text
+            Dim sql As String = "UPDATE tbl_mahasiswa SET npm = ?, nama = ?, alamat = ?, jenis_kelamin = ? WHERE id = ?"
+            Try
+                Call koneksi()
+                Dim cmd As New MySqlCommand(sql, conn)
+                cmd.Parameters.AddWithValue("@npm", input_npm)
+                cmd.Parameters.AddWithValue("@nama", input_nama)
+                cmd.Parameters.AddWithValue("@alamat", input_alamat)
+                cmd.Parameters.AddWithValue("@jenis_kelamin", input_jenis_kelamin)
+                cmd.Parameters.AddWithValue("@id", idToUpdate) ' Menggunakan id yang telah dipilih
+                cmd.ExecuteNonQuery()
+                MessageBox.Show("Data berhasil diupdate.")
+                tampildata()
+                bersihkan()
+            Catch ex As Exception
+                MessageBox.Show("Terjadi kesalahan: " & ex.Message)
+            Finally
+                conn.Close()
+            End Try
+        Else
+            MessageBox.Show("Pilih baris yang ingin diupdate terlebih dahulu.")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Call hapusdata()
+        Call bersihkan()
+    End Sub
+End Class
